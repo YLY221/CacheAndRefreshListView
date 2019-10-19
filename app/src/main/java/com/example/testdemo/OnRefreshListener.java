f1e4c0da65893aaa183e1c338f5625f68890d297
yaoliangyong <yaoliangyong221@163.com>
2019-10-19 22:58:05 +0800

    第一次创建并提交

---
+++ app/src/main/java/com/example/testdemo/OnRefreshListener.java
@@ -0,0 +1,14 @@
+package com.example.testdemo;
+
+public interface OnRefreshListener {
+
+    /**
+     * 下拉刷新
+     */
+    void onDownPullRefresh();
+
+    /**
+     * 上拉加载更多
+     */
+    void onLoadingMore();
+}
