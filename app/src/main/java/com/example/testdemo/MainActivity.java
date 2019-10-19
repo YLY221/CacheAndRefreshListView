d2ede94c4c19f58bac4bd13c2d5e2ad20ff3b92a
yaoliangyong <yaoliangyong221@163.com>
2019-10-19 23:31:16 +0800

    第三次提交

--- app/src/main/java/com/example/testdemo/MainActivity.java
+++ app/src/main/java/com/example/testdemo/MainActivity.java
@@ -16,23 +16,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnRefreshListener{
-
-    private List<String> textList;
-    private MyAdapter adapter;
     private RefreshListView rListView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rListView = (RefreshListView) findViewById(R.id.list_view);
-        textList = new ArrayList<String>();
-        for (int i = 0; i < 25; i++) {
-            textList.add("这是一条ListView的数据" + i);
-        }
-        adapter = new MyAdapter();
-        rListView.setAdapter(adapter);
+        ImageAdapter imageAdapter=new ImageAdapter(this,0,Images.imageUrls);
+        rListView.setAdapter(imageAdapter);
 
         //可以用这种注册监听的方式,这种方式需要实现MainActivity  implements OnRefreshListener
         rListView.setOnRefreshListener(this);
@@ -87,40 +78,6 @@ public class MainActivity extends AppCompatActivity implements OnRefreshListener
         });*/
     }
 
-    private class MyAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return textList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            // TODO Auto-generated method stub
-            return textList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            // TODO Auto-generated method stub
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
-            TextView textView = new TextView(MainActivity.this);
-            textView.setText(textList.get(position));
-            textView.setTextColor(Color.BLACK);
-            textView.setTextSize(18.0f);
-            return textView;
-        }
-
-    }
-
-
-
     @Override
     public void onDownPullRefresh() {
         new AsyncTask<Void, Void, Void>() {
@@ -128,15 +85,13 @@ public class MainActivity extends AppCompatActivity implements OnRefreshListener
             @Override
             protected Void doInBackground(Void... params) {
                 SystemClock.sleep(2000);
-                for (int i = 0; i < 2; i++) {
-                    textList.add(0, "这是下拉刷新出来的数据" + i);
-                }
+
                 return null;
             }
 
             @Override
             protected void onPostExecute(Void result) {
-                adapter.notifyDataSetChanged();
+                //adapter.notifyDataSetChanged();
                 rListView.hideHeaderView();
             }
         }.execute(new Void[]{});
@@ -150,15 +105,12 @@ public class MainActivity extends AppCompatActivity implements OnRefreshListener
             protected Void doInBackground(Void... params) {
                 SystemClock.sleep(5000);
 
-                textList.add("这是加载更多出来的数据1");
-                textList.add("这是加载更多出来的数据2");
-                textList.add("这是加载更多出来的数据3");
                 return null;
             }
 
             @Override
             protected void onPostExecute(Void result) {
-                adapter.notifyDataSetChanged();
+                //adapter.notifyDataSetChanged();
 
                 // 控制脚布局隐藏
                 rListView.hideFooterView();
